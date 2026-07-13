1df6d19eda079aa9166faf5741151fdf04f4d337
hr alamin <hralamin2020@gmail.com>
2026-07-13 14:52:04 +0600

    v1.0

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,23 +3,13 @@ import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
 
-// window.Echo = new Echo({
-//     broadcaster: 'reverb',
-//     key: import.meta.env.VITE_REVERB_APP_KEY,
-//     wsHost: import.meta.env.VITE_REVERB_HOST,
-//     wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-//     wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-//     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'http') === 'https',
-//     enabledTransports: ['ws', 'wss'],
-//     disableStats: true,
-// });
-
 window.Echo = new Echo({
     broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY ?? 768748,
-    wsHost: window.location.hostname,
-    wsPort: 8888,
-    wssPort: 8888,
-    forceTLS: window.location.protocol === 'https:',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'http') === 'https',
     enabledTransports: ['ws', 'wss'],
+    disableStats: true,
 });
